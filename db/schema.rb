bed28a8da40c1ce09c235f69531853a81028db60
Rebekah Liu <liu.rebekah@gmail.com>
2017-09-25 10:06:00 -0700

    add db/model validations for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925052925) do
+ActiveRecord::Schema.define(version: 20170925170439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170925052925) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "projects", force: :cascade do |t|
