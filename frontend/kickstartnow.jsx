d47f9715bc39c4b44b0918f300148b763899f03c
Rebekah Liu <liu.rebekah@gmail.com>
2017-09-29 15:30:12 -0700

    removed actions from window

--- frontend/kickstartnow.jsx
+++ frontend/kickstartnow.jsx
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-//for testing//
-import {receiveProjects, receiveProject, logout, resetProject} from './actions/project_actions';
-import {getProjects, getProject, createProject, destroyProject} from './actions/project_actions';
-import {postBacking} from './util/backing_api_util';
-import {search} from './util/search_api_util';
-//for testing//
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const preloadedState = {};
@@ -20,17 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const store = configureStore(preloadedState);
 
-  //for testing//
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.getProjects = getProjects;
-  window.getProject = getProject;
-  window.createProject = createProject;
-  window.destroyProject = destroyProject;
-  window.postBacking = postBacking;
-  window.resetProject = resetProject;
-  window.search = search;
-  //for testing//
 
   ReactDOM.render(<Root store={store}/>, root);
 });
